4094f2a6c25fb7b7c41ef8cb233a74d562e5f94e
testrestpest <k.abrahams@live.co.uk>
2025-06-21 09:47:38 +0100

    fix: reduce click-and-collect section padding on midsize and mobile screens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -62,7 +62,7 @@ function Home() {
 
       {/* Section 2 */}
       {pageContent.section_2_title && (
-        <section className="click-and-collect py-16" style={{ paddingBottom: '200px' }}>
+        <section className="click-and-collect">
           <div className="container text-center">
             <h2 className="section-title mb-8">
               {pageContent.section_2_title}
